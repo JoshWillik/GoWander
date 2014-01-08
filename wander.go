1276bb43844c5bc6b845ca1f67fb4c8530f6e002
Josh Vanderwillik <joshwillik@gmail.com>
2014-01-08 10:33:24 -0600

    We now render a red triangle with a pulsing background

--- wander.go
+++ wander.go
@@ -4,14 +4,12 @@ import (
     f "fmt"
     "github.com/go-gl/gl"
     glfw "github.com/go-gl/glfw3"
-    "encoding/binary"
+    "math"
+    "time"
 )
 
 var (
-    triangle = []float32{
-        0.0, -0.5,
-        0.5, -0.5,
-        -0.5, -0.5}
+    seconds = time.Now()
 )
 
 func main(){
@@ -37,26 +35,70 @@ func main(){
     if gl.Init() != 0 {
         panic("Failed to init GL")
     }
-    gl.ClearColor(0.0, 0.45, 0.0, 0.0)
 
-    buf := newBuffer()
-    defer buf.Delete()
-
-    prog := newProgram()
+    prog := setupProgram()
     defer prog.Delete()
-    f.Println(prog.GetInfoLog())
-    attrib := prog.GetAttribLocation("position")
+    prog.Use()
 
+    arr := getVertexArray()
+    defer arr.Delete()
+    arr.Bind()
+
+    setup()
     for !window.ShouldClose() {
-        attrib.EnableArray()
-        attrib.AttribPointer(2, gl.FLOAT, false, 0, nil)
         draw()
         animate()
-        attrib.DisableArray()
         window.SwapBuffers()
         glfw.PollEvents()
     }
 
+}
+func setup(){
+    //gl.PointSize(40.0)
+}
+func setupProgram()(prog gl.Program){
+    vertexSource := `
+        #version 430 core
+        
+        layout (location = 0) in vec4 offset;
+
+        const vec4 vertecies[3] = vec4[3](
+            vec4(0.25, 0.5, 0.5, 1.0),
+            vec4(-0.25, 0.5, 0.5, 1.0),
+            vec4(-0.25, -0.5, 0.5, 1.0)
+        );
+        
+        void main(){
+            gl_Position = vertecies[gl_VertexID];
+        }`
+    fragmentSource := `
+        #version 430 core
+        
+        out vec4 color;
+
+        void main(){
+            color = vec4(1.0, 0.0, 0.0, 0.0); // red, blue, green, ??
+        }`
+    vert, frag := gl.CreateShader(gl.VERTEX_SHADER), gl.CreateShader(gl.FRAGMENT_SHADER)
+    defer vert.Delete()
+    defer frag.Delete()
+    vert.Source(vertexSource)
+    frag.Source(fragmentSource)
+    vert.Compile()
+    frag.Compile()
+
+    prog = gl.CreateProgram()
+    prog.AttachShader(vert)
+    prog.AttachShader(frag)
+    prog.Link()
+    prog.Use()
+    f.Println(prog.GetInfoLog())
+
+    return
+}
+func getVertexArray()(arr gl.VertexArray){
+    arr = gl.GenVertexArray()
+    return
 }
 
 func key(window *glfw.Window, k glfw.Key, s int, action glfw.Action, mods glfw.ModifierKey) {
@@ -75,56 +117,23 @@ func key(window *glfw.Window, k glfw.Key, s int, action glfw.Action, mods glfw.M
 func reshape(window *glfw.Window, width, height int){
     gl.Viewport(0, 0, width, height)
 }
-
-func newBuffer() (buf gl.Buffer){
-    buf = gl.GenBuffer()
-    buf.Bind(gl.ARRAY_BUFFER)
-    gl.BufferData(gl.ARRAY_BUFFER, binary.Size(triangle), triangle, gl.STATIC_DRAW)
-
-    return buf
-}
-func newShader(shaderType string) (shade gl.Shader){
-    var shaderString string
-
-    switch shaderType {
-        case "vertex":
-            shade = gl.CreateShader(gl.VERTEX_SHADER)
-            shaderString = `
-            #version 150
-
-            in vec2 position;
-
-            void main(){
-                gl_Position = vec4(position, 0.0, 1.0);
-            }`
-            break;
-        case "fragment":
-            shade = gl.CreateShader(gl.FRAGMENT_SHADER)
-            shaderString = `
-            #version 150
-
-            out vec4 outColor;
-            
-            void main(){
-                outColor = vec4(1.0, 1.0, 1.0, 1.0);
-            }`
-            break;
-    }
-    shade.Source(shaderString)
-    shade.Compile()
-    return
-}
-func newProgram()(prog gl.Program){
-    prog = gl.CreateProgram()
-    prog.AttachShader(newShader("vertex"))
-    prog.AttachShader(newShader("fragment"))
-    prog.Link()
-    return
-}
 func draw(){
     gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
     gl.DrawArrays(gl.TRIANGLES, 0, 3)
 }
 
 func animate(){
+    now := float64(time.Since(seconds))
+
+//    offset = []float64{
+//        math.Sin(now),
+//        math.Cos(now),
+//        0.0,0.0}
+
+    red := gl.GLclampf(math.Sin(now) * 0.25 + 0.75)
+    blue := gl.GLclampf(math.Cos(now) * 0.25 + 0.75)
+    green := gl.GLclampf(time.Since(seconds))
+    _ = green;
+
+    gl.ClearColor(red, blue, 0.2, 0.0)
 }
